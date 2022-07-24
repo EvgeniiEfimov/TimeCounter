cdb033f3189690d6337872cae3eb245e9af2342b
Евгений Ефимов <Jeki_97@mail.ru>
2022-07-24 11:57:50 +0600

    add image infoAppVC

--- kfcMyTime/Controllers/AppInfoViewController.swift
+++ kfcMyTime/Controllers/AppInfoViewController.swift
@@ -8,9 +8,28 @@
 import UIKit
 
 class AppInfoViewController: UIViewController {
-
+    @IBOutlet weak var pageControlOutlet: UIPageControl!
+    @IBOutlet weak var imageViewOutlet: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+    }
+    
+    @IBAction func swipeHandler(_ gestureRecognizer : UISwipeGestureRecognizer) {
+        if gestureRecognizer.state == .ended {
+            print("true")
+            if pageControlOutlet.currentPage < pageControlOutlet.numberOfPages - 1 {
+            pageControlOutlet.currentPage += 1
+            } else {
+                pageControlOutlet.currentPage = 0
+            }
+        }
+        if pageControlOutlet.currentPage == 0 {
+            imageViewOutlet.image = UIImage(named: "infoAdd")
+        }
+        if pageControlOutlet.currentPage == 1 {
+            imageViewOutlet.image = UIImage(named: "addIDataInfo")
+        }
     }
 }
